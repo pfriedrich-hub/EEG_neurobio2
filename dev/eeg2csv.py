49fd825c44315f45554ad34fac33de40be968db0
paulfriedrich <paul.friedr1@gmail.com>
2024-03-15 12:25:15 +0100

    add raw format option to eeg2csv output

--- dev/eeg2csv.py
+++ dev/eeg2csv.py
@@ -9,10 +9,10 @@ electrode_names = json.load(open(Path.cwd() / 'resources' / 'misc' / "electrode_
 
 # write data to csv
 raw = mne.io.read_raw_brainvision(header_file, preload=True)
-raw = raw.filter(l_freq=.5, h_freq=None)
+raw = raw.filter(l_freq=.5, h_freq=None)  # remove strong drifts to make the data easier to inspect for a start
 start, window_size = 50000, 500  # only use a slice of the raw data
 
-eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size])  # timepoints as the column names
+eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size])
 
 eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size] * 10e6,    # values, convert to µV
             index=electrode_names.values(),    # channel names index
