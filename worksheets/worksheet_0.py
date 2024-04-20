c8df8e5c99c65dbf0f1da81778e9fb393f7c8b59
paulfriedrich <paul.friedr1@gmail.com>
2024-04-20 16:08:21 +0200

    add de-noising by average exercise

--- worksheets/worksheet_0.py
+++ worksheets/worksheet_0.py
@@ -2,14 +2,14 @@
 import numpy
 from matplotlib import pyplot as plt
 
-# --- exercise: make some noise!
+# --- exercise 1: make some noise! --- #
 
 # sampling rate of the signal (in Hz: samples per second)
-samplerate = 200
+samplerate = 500
 
 # duration of the signal (in seconds)
 duration = 1
-signal_length = samplerate * duration
+signal_length = duration * samplerate
 
 # create a time axis (array of time points)
 time = numpy.linspace(start=0, stop=duration, num=signal_length)  # start, stop, number of steps
@@ -21,12 +21,37 @@ noise = numpy.random.rand(signal_length)
 plt.plot(time, noise)
 plt.xlabel('Time (s)')  # label the time axis with a string
 
-# --- exercise: making waves
+
+# --- exercise 2: de-noising by averaging many signals --- #
+
+# create multiple noisy signals:
+n = _  # choose how many signals you want to generate (try different values and compare the outcome)
+
+signals_list = list()  # create an empty list to store some signals
+for i in range(n):  # generate n signals and add them to the list
+    noise = numpy.random.rand(signal_length)  # create different random noise signals
+    signals_list.append(noise)  # add each signal to the signals_list
+
+# generate a figure to plot the noise signals
+plt.figure(figsize=(15, 4))
+plt.xlabel('Time (s)')
+
+# plot the first 10 noise signals from the matrix for comparison
+for i in range(n):
+    plt.plot(time, signals_list[i], color='grey')
+
+# average across noise signals
+mean_signal = numpy.mean(signals_list, axis=0)
+# plot the averaged noise to compare it to the original noise
+plt.plot(time, mean_signal, color='black')
+
+
+# --- exercise 3: making waves
 
 # parameters of the sine wave:
 amplitude = 1
 frequency = 4
-phi = numpy.sin(0)  # phase angle offset 0-360°
+phi = numpy.sin(0)  # phase angle (0°-360°)
 
 # create a wave:
 wave = amplitude * numpy.sin(2 * numpy.pi * frequency * time + phi)
@@ -46,7 +71,14 @@ for freq in frequencies:
     sine_waves.append(x)
 sum = numpy.sum(sine_waves, axis=0)
 
-# --- exercise: load eeg data
+# plot spectrum here?
+
+# create a difference wave
+
+
+# --- exercise: load raw eeg data
+import mne
+raw = mne.io.read_raw_brainvision('file path')
 
 # --- exercise: select data by time interval
 
