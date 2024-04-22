ad1212b23c64f64cfa991ea1e0da26dbd612fff9
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2024-04-22 09:40:01 +0200

    Worksheet 1 and 2

--- dev/eeg2csv.py
+++ dev/eeg2csv.py
@@ -9,15 +9,15 @@ electrode_names = json.load(open(Path.cwd() / 'resources' / 'misc' / "electrode_
 
 # write data to csv
 raw = mne.io.read_raw_brainvision(header_file, preload=True)
-raw = raw.filter(l_freq=.5, h_freq=None)  # remove strong drifts to make the data easier to inspect for a start
+# raw = raw.filter(l_freq=.5, h_freq=None)  # remove strong drifts to make the data easier to inspect for a start
 start, window_size = 50000, 500  # only use a slice of the raw data
 
 eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size])
 
-eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size] * 10e6,    # values, convert to µV
-            index=electrode_names.values(),    # channel names index
-            columns=raw.times[:window_size])  # timepoints as the column names
+eeg_data = pandas.DataFrame(data=raw._data[:, start:start+window_size],    # values, convert to µV
+            index=electrode_names.keys())    # channel names index
+            # columns=raw.times[:window_size])  # timepoints as the column names
 
-eeg_data.to_csv(Path.cwd() / 'resources' / 'EEG_data.csv', float_format='%.3f')
+eeg_data.to_csv(Path.cwd() / 'resources' / 'EEG_data.csv')
 
 eeg_data = pandas.read_csv(Path.cwd() / 'resources' / 'EEG_data.csv')
