19b20bd9904af205218c1986c2725033b3207652
suongwelp <suongwelp@gmail.com>
2025-04-28 15:30:31 +0200

    finalize oddball experiment

--- experiments/oddball_eeg.py
+++ experiments/oddball_eeg.py
@@ -4,89 +4,98 @@ import random
 import time
 import freefield
 from pathlib import Path
-fs = 48828
+fs = 24414
 slab.set_default_samplerate(fs)
 data_dir = Path.cwd() / 'experiments'
-rcx_file = 'oddball.rcx'
+rcx_file = 'play_buf.rcx'
 
 # generate standard
 f0 = 500
 n_harmonics = 3
 duration = 0.075
-level = 75
+level = 80
 
+# generate standard - EEG trigger code 1
 standard = slab.Sound.tone(f0, duration, level=level)
 standard += slab.Sound.tone(f0*2, duration, level=level-3)
 standard += slab.Sound.tone(f0*3, duration, level=level-6)
-standard.ramp(duration=.005)
 
-# generate deviant 1 - frequency deviant
+# generate frequency deviant - EEG trigger code 2
 dev_f0 = 550
 deviant_1 = slab.Sound.tone(dev_f0, duration, level=level)
 deviant_1 += slab.Sound.tone(dev_f0*2, duration, level=level-3)
 deviant_1 += slab.Sound.tone(dev_f0*3, duration, level=level-6)
-deviant_1.ramp(duration=.005)
 
-# generate deviant 2 - loudness deviant
-dev_level = 85
+# generate loudness deviant - EEG trigger code 3
+dev_level = level + 10
 deviant_2 = slab.Sound.tone(f0, duration, level=dev_level)
 deviant_2 += slab.Sound.tone(f0*2, duration, level=dev_level-3)
 deviant_2 += slab.Sound.tone(f0*3, duration, level=dev_level-6)
-deviant_2.ramp(duration=.005)
 
-# generate deviant 3 - duration deviant
-dev_duration = 0.025
+# generate duration deviant - EEG trigger code 4
+dev_duration = duration - 0.05
 deviant_3 = slab.Sound.tone(f0, dev_duration, level=level)
 deviant_3 += slab.Sound.tone(f0*2, dev_duration, level=level-3)
 deviant_3 += slab.Sound.tone(f0*3, dev_duration, level=level-6)
-deviant_3.ramp(duration=.005)
 silence = slab.Sound.silence(duration=.05)
 deviant_3 = slab.Sound.sequence(deviant_3, silence)
 
-#  deviant 4 - location deviant
-# handle in rcx
+# location deviant  - EEG trigger code 5
+# handled in rcx
 
 # (SOA) of 500 ms in three 5 min sequences
-def run_experiment(
-        n_trials=1845,
-        soa = 0.5,
-        save_csv_path=Path.cwd() / 'data' / 'mmn_trials.csv'):
+def run_experiment(n_trials=1845, soa = 0.5):
+    # stimulus codes (0 - 4) appear in the trial sequence
+    # eeg triggers: 1 - 5
+    # 1: standard, 2 - 5: deviants
+    # write stimulus data to buffers
+    freefield.write('playbuflen', standard.n_samples, ['RX81', 'RX82'])
+    freefield.write('std_data', standard.data, ['RX81', 'RX82'])
+    freefield.write('dev_data_1', deviant_1.data, ['RX81'])
+    freefield.write('dev_data_2', deviant_2.data, ['RX81'])
+    freefield.write('dev_data_3', deviant_3.data, ['RX81'])
 
-    # stimulus codes (0 - 4) appear in the trial sequence and eeg triggers
-    # 0: standard, 1 - 4: deviants
+    freefield.write('dev_chan', 14, ['RX82'])
 
     # generate trial sequence
     sequence = generate_mmn_sequence(n_trials)
-    print(sequence)
     trial_sequence = slab.Trialsequence(conditions=sequence)
     trial_sequence.trials = numpy.arange(n_trials).tolist()
-    trial_sequence.save_csv(save_csv_path)    # Save to CSV
+    # trial_sequence.save_csv(save_csv_path)    # Save to CSV
 
-    # write stimulus data to buffers
-    freefield.write('data_std', standard.data, ['RX81', 'RX82'])
-    freefield.write('data_dev_1', deviant_1.data, ['RX81', 'RX82'])
-    freefield.write('data_dev_2', deviant_2.data, ['RX81', 'RX82'])
-    freefield.write('data_dev_3', deviant_3.data, ['RX81', 'RX82'])
-    freefield.write('data_dev_4', standard.data, ['RX81', 'RX82'])
-    freefield.write('n_samples', standard.n_samples, ['RX81', 'RX82'])
-    [speaker] = freefield.pick_speakers(23)
-    freefield.set_speaker(speaker)
+    stim_codes = [0, 1, 2, 3]
+    channels=['std_chan', 'dev_chan_1', 'dev_chan_2', 'dev_chan_3']
 
     # Run trials
     print("\nStarting MMN experiment...\n")
-    for stim_code in (trial_sequence):
-        print(f"Trial {trial_sequence.this_n}, stim code: {trial_sequence.this_trial}")
-        freefield.write('stim_code', stim_code, ['RX81', 'RX82'])
-        if stim_code == 4:
-            freefield.set_speaker(44)  # play at 52.5° azimuth
 
-        freefield.play('zBusA')
-        time.sleep(soa - standard.duration)
+    for stim_code in (trial_sequence):
+        freefield.write('trigcode', stim_code, ['RX81', 'RX82'])
+        # halt every 615 stmiuli
+        if stim_code == 4:  # play deviant location
+            freefield.write('std_chan', 14, ['RX82'])
+            for code in stim_codes:  # set all other channels to 25
+                freefield.write(channels[code], 25, ['RX81'])
+            freefield.play('zBusA')
+            freefield.wait_to_finish_playing()
+            freefield.write('std_chan', 25, ['RX82'])
+
+        else:  # play standard location
+            freefield.write(channels[stim_code], 1, ['RX81'])
+            for code in list(filter(lambda x: x != stim_code, stim_codes)):  # set all other channels to 25
+                freefield.write(channels[code], 25, ['RX81'])
+            freefield.play('zBusA')
+            freefield.wait_to_finish_playing()
+
+        time.sleep(soa-standard.duration)
+        if trial_sequence.this_n == 615:
+            input('Press enter to continue...')
+
     print("\nExperiment complete.")
 
 def init_dsp(rcx_file):
-    proc_list = [['RX81', 'RX8', data_dir / 'rcx' / rcx_file],
-                 ['RX82', 'RX8', data_dir / 'rcx' / rcx_file]]
+    proc_list = [['RX81', 'RX8', data_dir / rcx_file],
+                 ['RX82', 'RX8', data_dir / rcx_file]]
     freefield.initialize('dome', device=proc_list, sensor_tracking=False)
     # freefield.load_equalization(data_dir / '')
 
@@ -123,15 +132,9 @@ def generate_mmn_sequence(n_trials, leading_standards=15):
         sequence.append(0)        # even index
     return sequence[:n_trials]
 
-
-# # Example usage
-# if __name__ == "__main__":
-#     init_dsp(rcx_file)
-#     run_experiment(
-#         deviant_freq=0.8,
-#         n_trials=300,
-#         isi=0.5,
-#         save_csv_path="mmn_trials.csv")
+if __name__ == "__main__":
+    init_dsp(rcx_file)
+    run_experiment()
 
 
 
